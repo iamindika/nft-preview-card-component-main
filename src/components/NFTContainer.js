cf73bb2cb218e5b43f14e72b2244153fb2fe8576
iamindika <ikolamba2@gmail.com>
2022-01-30 14:10:29 -0500

    define and render NFTDesc component

--- src/components/NFTContainer.js
+++ src/components/NFTContainer.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NFTLogo from './NFTLogo';
+import NFTDesc from './NFTDesc';
 
 export default function NFTContainer() {
   return (
     <article className="container">
       <NFTLogo />
+      <NFTDesc />
     </article>
   )
 }
---
+++ src/components/NFTDesc.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NFTDesc() {
+  return (
+    <section className="nft-desc">
+      <h1 className="nft-desc__title">Equilibrium #3429</h1>
+      <p className="nft-desc__text">Our Equilibrium collection promotes balance and calm.</p>
+    </section>
+  )
+}
