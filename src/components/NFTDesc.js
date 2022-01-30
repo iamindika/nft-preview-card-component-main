a14ca1d11860e991de349edd6e113f983c13322e
iamindika <ikolamba2@gmail.com>
2022-01-30 18:06:17 -0500

    define reusable css class 'blur' and apply to NFTDesc component

--- src/components/NFTDesc.js
+++ src/components/NFTDesc.js
@@ -4,7 +4,7 @@ export default function NFTDesc() {
   return (
     <section className="nft-desc">
       <h1 className="nft-desc__title">Equilibrium #3429</h1>
-      <p className="nft-desc__text">Our Equilibrium collection promotes balance and calm.</p>
+      <p className="nft-desc__text blur">Our Equilibrium collection promotes balance and calm.</p>
     </section>
   )
 }
