57cb80890933373c5bc0edb15d4c8127257106d0
iamindika <ikolamba2@gmail.com>
2022-01-30 19:01:05 -0500

    define and render NFTStats component

--- src/components/NFTDesc.js
+++ src/components/NFTDesc.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import NFTStats from './NFTStats';
 
 export default function NFTDesc() {
   return (
     <section className="nft-desc">
       <h1 className="nft-desc__title">Equilibrium #3429</h1>
       <p className="nft-desc__text blur">Our Equilibrium collection promotes balance and calm.</p>
+      <NFTStats />
     </section>
   )
 }
