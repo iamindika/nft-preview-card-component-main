357329fc88d9ebe1e2e2eb0b90afd82d846f4cf1
iamindika <ikolamba2@gmail.com>
2022-01-31 00:54:20 -0500

    add bg container for view icon

--- src/components/NFTLogo.js
+++ src/components/NFTLogo.js
@@ -9,20 +9,22 @@ export default function NFTLogo() {
         src={logo}
         alt="nft equilibrium logo"
       />
-     <svg 
-        className="nft-banner__view"
-        width="48" 
-        height="48" 
-        xmlns="http://www.w3.org/2000/svg"
-      ><g 
-        fill="none" 
-        fill-rule="evenodd">
-          <path 
-            d="M0 0h48v48H0z"/>
-          <path  
-            d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z" 
-            fill="#FFF" 
-            fill-rule="nonzero"/></g></svg>
+      <div className="nft-banner__view-bg">
+        <svg 
+          className="nft-banner__view-icon"
+          width="48" 
+          height="48" 
+          xmlns="http://www.w3.org/2000/svg"
+        ><g 
+          fill="none" 
+          fill-rule="evenodd">
+            <path 
+              d="M0 0h48v48H0z"/>
+            <path  
+              d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z" 
+              fill="#FFF" 
+              fill-rule="nonzero"/></g></svg>
+      </div>
     </header>
   )
 }
